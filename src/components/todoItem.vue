5ee368f42583e70438f09b90d6f9688df792d880
ours-is-the-fury <783555989@qq.com>
2018-06-22 12:50:15 +0800

    learn vue

--- src/components/todoItem.vue
+++ src/components/todoItem.vue
@@ -1,12 +1,22 @@
 <template>
-<li>item</li>
+<li class="item" @click="handleDelete">{{content}}</li>
 </template>
 
 <script>
-
+export default{
+    props: ['content','index'], //接收数据定义
+    methods:{
+        handleDelete(){
+            this.$emit('delete',this.index)
+        }
+    }
+}
 </script>
 
 
-<style scoped>
+<style scoped> /*scoped 定义局部作用域*/
+.item{
+    color: green 
+}
 
 </style>
--- src/todoList.vue
+++ src/todoList.vue
@@ -1,34 +1,44 @@
 <template>
-  <div>
+  <div><!--只能有一个包裹元素-->
     <div>
-      <input v-model="inputValue"/>
-      <button @click="handleSubmit">tijiao</button>
+      <input  v-model = "inputValue" />
+      <button @click = "handleSubmit">tijiao</button>
     </div>
     <ul>
-      <todo-item
-      v-for=""
+      <todo-item 
+      v-for= "(item , index) of list"
+      :key = "index"
+      :content = "item"
+      :index= "index"
+      @delete= "handleDelete" 
+      ></todo-item>
     </ul>
   </div>
 </template>
 
 <script>
-import todoItem from'./components/todoItem'
-export default{
-  components:{
-    'todo-item':todoItem
+import todoItem from "./components/todoItem";
+export default {
+  components: {
+    "todo-item": todoItem
   },
-  data (){
-    return{
-      inputValue:''
-    }
+  data() {
+    //以前是对象，现在应该是一个函数
+    return {
+      inputValue: "",
+      list: []
+    };
   },
-  methods:{
-    handleSubmit(){
-      this.list.push(this.inputValue)
-      this.inputValue=''
+  methods: {
+    handleSubmit() {
+      this.list.push(this.inputValue); //list 自动寻找list
+      this.inputValue = "";
+    },
+    handleDelete (index){
+      this.list.splice(index, 1)
     }
   }
-}
+};
 </script>
 
 <style>
